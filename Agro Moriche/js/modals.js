23ce2185767408db7f806f024245cc0b1e5b39e8
AZRA-444 <angel31490@gmail.com>
2026-05-27 15:31:55 -0400

    Add files via upload

---
+++ Agro Moriche/js/modals.js
@@ -0,0 +1,27 @@
+
+function abrirModal(boton) {
+
+    const card = boton.closest('.cards');
+
+
+    const imgBlock = card.querySelector('.img-producto').cloneNode(true);
+    const contentBlock = card.querySelector('.card-content').cloneNode(true);
+    const modalContentBlock = card.querySelector('.modal-content').cloneNode(true);
+
+    const modal = document.getElementById('productModal'); 
+    const modalBody = document.getElementById('modalDynamicBody');
+
+    modalBody.innerHTML = '';
+
+    modalBody.append(imgBlock, contentBlock, modalContentBlock);
+
+    modalContentBlock.style.display = 'block';
+    modal.classList.add('active');
+    document.body.style.overflow = 'hidden'; 
+}
+
+function cerrarModal() {
+    const modal = document.getElementById('productModal');
+    modal.classList.remove('active');
+    document.body.style.overflow = '';
+}
